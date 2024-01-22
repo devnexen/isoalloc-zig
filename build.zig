1245e42e5af7959059d3eec865e1a531e09697c0
David Carlier <devnexen@gmail.com>
2024-01-22 13:21:39 +0000

    fix build.
    TODO, modernize api for 0.11 and above.

--- build.zig
+++ build.zig
@@ -1,11 +1,11 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardOptimizeOption(.{});
-    const target = b.standardTargetOptions(.{});
-    const output = b.addStaticLibrary(.{ .name = "isoalloc-zig", .root_source_file = .{ .path = "src/isoalloc.zig" }, .target = target, .optimize = mode });
+    const mode = b.standardReleaseOptions();
+    const output = b.addStaticLibrary("isoalloc-zig", "src/isoalloc.zig");
     output.linkLibC();
     // TODO trying local build as options are compile time.
     output.linkSystemLibrary("isoalloc");
-    b.installArtifact(output);
+    output.setBuildMode(mode);
+    output.install();
 }
--- src/isoalloc.zig
+++ src/isoalloc.zig
@@ -1,26 +1,27 @@
 const std = @import("std");
+const c = @cImport(@cInclude("iso_alloc.h"));
 const mem = std.mem;
 const math = std.math;
-const panic = std.debug.panic;
+const debug = std.debug;
 const Allocator = mem.Allocator;
 
-var isoalloc_allocator_fn = Allocator{ .allocFn = isoallocAlloc, .resizeFn = isoallocResize };
+var isoalloc_allocator_fn = Allocator.init([*]u8, isoallocAlloc, isoallocResize, isoallocFree); //Allocator{ .allocFn = isoallocAlloc, .resizeFn = isoallocResize };
 pub export const isoalloc_allocator = &isoalloc_allocator_fn;
 
-fn isoallocAlloc(self: *Allocator, len: usize, ptr_align: u29, len_align: u29, ret_addr: usize) Allocator.Error![]u8 {
+fn isoallocAlloc(len: usize, ptr_align: u29, _: u29, _: usize) Allocator.Error![]u8 {
     if (len == 0)
-        return null;
+        return error.OutOfMemory;
     // just for the principle ..
     if (!math.isPowerOfTwo(ptr_align)) {
-        panic.print("alignment must be a power of 2 {}", .{ptr_align});
-        return null;
+        debug.print("alignment must be a power of 2 {}", .{ptr_align});
+        return error.OutOfMemory;
     }
 
-    var p: [*]u8 = @as([*]u8, @ptrCast(c.iso_alloc(len))) orelse return error.OutOfMemory;
-    return ptr[0..len];
+    var p: [*]u8 = @ptrCast([*]u8, c.iso_alloc(len));
+    return p[0..len];
 }
 
-fn isoallocResize(self: *Allocator, p: []u8, ptr_align: u29, len: usize, len_align: u29, ret_addr: usize) Allocator.Error!usize {
+fn isoallocResize(p: []u8, _: u29, len: usize, len_align: u29, _: usize) Allocator.Error!usize {
     if (len == 0) {
         c.iso_free(p.ptr);
         return 0;
@@ -28,8 +29,12 @@ fn isoallocResize(self: *Allocator, p: []u8, ptr_align: u29, len: usize, len_ali
 
     if (len <= p.len)
         return mem.alignAllocLen(p.len, len, len_align);
-    const new_len = c.iso_usable_size(p.ptr);
-    if (new_len <= buf.len)
+    const new_len = c.iso_chunksz(p.ptr);
+    if (new_len <= p.len)
         return mem.alignAllocLen(p.len, new_len, len_align);
     return error.OutOfMemory;
+}
+
+fn isoallocFree(p: []u8, _: u29, _: usize) void {
+    c.iso_free(p.ptr);
 }
