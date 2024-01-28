1fdd0525bcb14da6730eaae72e4dffce49534590
David Carlier <devnexen@gmail.com>
2024-01-28 13:23:18 +0000

    forgotten unit tests

---
+++ src/ut.zig
@@ -0,0 +1,24 @@
+const ut = @import("std").testing;
+const iso = @import("isoalloc.zig").isoalloc_allocator;
+
+test "basic alloc" {
+    const P = struct {
+        const Self = @This();
+        w: u32,
+        h: u32,
+        const Buf = struct {
+            priv: [*]u8,
+            next: ?*Buf = null,
+        };
+    };
+
+    const ptr: []P = iso.alloc(P, 1) catch @panic("test failure");
+    iso.free(ptr);
+}
+
+test "resize it" {
+    const buf: []u8 = iso.alloc(u8, 16) catch @panic("test failure");
+    if (iso.resize(buf, 1024))
+        @panic("test failure");
+    iso.free(buf);
+}
