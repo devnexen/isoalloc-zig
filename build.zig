b4e93ff947d3607c4049c68fde9527852aaba755
David Carlier <devnexen@gmail.com>
2024-07-05 00:14:25 +0100

    update build fir zig 0.13

--- build.zig
+++ build.zig
@@ -1,15 +1,15 @@
-const Builder = @import("std").build.Builder;
+const Builder = @import("std").Build;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-    const output = b.addStaticLibrary(.{ .name = "isoalloc-zig", .root_source_file = .{ .path = "src/isoalloc.zig" }, .target = target, .optimize = mode });
+    const output = b.addStaticLibrary(.{ .name = "isoalloc-zig", .root_source_file = b.path("src/isoalloc.zig") , .target = target, .optimize = mode });
     output.linkLibC();
     // TODO trying local build as options are compile time.
     output.linkSystemLibrary("isoalloc");
     b.installArtifact(output);
 
-    const ut = b.addTest(.{ .name = "isoalloc-ut", .root_source_file = .{ .path = "src/ut.zig" }, .target = target, .optimize = mode });
+    const ut = b.addTest(.{ .name = "isoalloc-ut", .root_source_file = b.path("src/ut.zig"), .target = target, .optimize = mode });
     ut.linkLibC();
     ut.linkSystemLibrary("isoalloc");
 
