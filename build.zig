03cb519d91d6a2ca24393973aa9e70fcf120e8cf
David Carlier <devnexen@gmail.com>
2024-07-05 00:15:26 +0100

    fmt..

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const Builder = @import("std").Build;
 pub fn build(b: *Builder) void {
     const mode = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-    const output = b.addStaticLibrary(.{ .name = "isoalloc-zig", .root_source_file = b.path("src/isoalloc.zig") , .target = target, .optimize = mode });
+    const output = b.addStaticLibrary(.{ .name = "isoalloc-zig", .root_source_file = b.path("src/isoalloc.zig"), .target = target, .optimize = mode });
     output.linkLibC();
     // TODO trying local build as options are compile time.
     output.linkSystemLibrary("isoalloc");
